5a4f76bd9b2507a3f3a825273125855bde335a34
唐朝迪 <tangchaodi@192.168.0.101>
2022-03-29 23:23:43 +0800

    init commit

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if(principal == null || principal <= 0){
+        alert("Enter a positive number");
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    // get the final result 
+    var result = "If you deposit \<mark\>" + principal+"\</mark\>,\<br\> at an interest rate of \<mark\>" + rate + "\</mark\>%\<br\>You will recieve an amount of \<mark\>"
+    + interest + "\</mark\>,\<br\>in the year \<mark\>" + year  + "\</mark\>\<br\>";
+    document.getElementById("result").innerHTML= result;
+
 }
-        
+
+function updateRate() {
+    var reteval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = reteval + "%";
+}
